b595db885292d370839b14d87928019783ed27c3
SteveCall <36867018+SteveCall@users.noreply.github.com>
2018-04-09 10:58:45 -0400

    updating js file

--- appTask2/appTask2.js
+++ appTask2/appTask2.js
@@ -1,14 +1,57 @@
-function testDom(){
+var buttontask = $("#buttontask");
+var task = $("#task");
+var list1 = $("#list1");
+var Delete = $("#delete");
+var Delete1 = $("#delete1");
 
-  let listOne = document.getElementById("listOne");
-  let inputField = document.getElementById("inputField");
+buttontask.on('click', function () {
+  var newE = $('<li></li>');
+  var textnode = $("#task").val();
+  var buttondelete = $('<div>X</div>');
 
-  $("buttonCreate").on("click", function(){
-    let newElem = document.createElement("list")
-    let newContent = document.createTextNode(inputField.value);
-  })
+  Delete.append(buttondelete);
+  newE.append(textnode);
 
-  $("buttonDelete").on("click", function(){
-    listOne.removeChild(newElem);
-  })
-}
+  newE.on('click', function() {
+    this.remove();
+  });
+
+  buttondelete.on('click', function(){
+    this.remove();
+    newE.remove();
+  });
+
+  newE.addClass(task.value);
+  list1.append(newE);
+
+  buttondelete.addClass('delete');
+});
+
+
+
+var buttondesc = $("#buttondesc");
+var list2 = $("#list2");
+var desc = $("#desc");
+
+buttondesc.on('click', function () {
+  var newElem = $('<li></li>');
+  var textnode1 = $("#desc").val();
+  var buttondelete1 = $('<div>X</div>');
+
+  Delete1.append(buttondelete1);
+  newElem.append(textnode1);
+
+  buttondelete1.on('click', function(){
+    this.remove();
+    newElem.remove();
+  });
+
+  newElem.on('click', function() {
+    this.remove();
+  });
+
+  newElem.addClass(desc.value);
+  list2.append(newElem);
+
+  buttondelete1.addClass('delete1');
+});
